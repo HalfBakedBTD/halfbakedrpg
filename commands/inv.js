7bacd113ffc7a22661011c9ad03d24e54e5f05e6
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-19 18:44:08 -0400

    Update inv.js

--- commands/inv.js
+++ commands/inv.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if(!inv[message.author.id]) return message.reply("Please use 'create' to make a charecter before using this command!");
   let botEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
-  .setDescription(`**${message.author.username}'s inventory:**\n\tApples - ${inv[message.author.id].apple}\n\tGrapes - ${inv[message.author.id].grape}\n\tTomatoes - ${inv[message.author.id].tomato}\n\tCorn - ${inv[message.author.id].corn}\n\tEggs - ${inv[message.author.id].egg}\n\n\tFists - ${inv[message.author.id].fists}\n\tDaggers - ${inv[message.author.id].dagger}\n\tSwords - ${inv[message.author.id].sword}\n\tMaces - ${inv[message.author.id].mace}`);
+  .setDescription(`**${message.author.username}'s inventory:**\n\tApples 🍎 - ${inv[message.author.id].apple}\n\tGrapes 🍇 - ${inv[message.author.id].grape}\n\tTomatoes 🍅 - ${inv[message.author.id].tomato}\n\tCorn 🌽 - ${inv[message.author.id].corn}\n\tEggs 🥚 - ${inv[message.author.id].egg}\n\n\tFists 🤜 - ${inv[message.author.id].fists}\n\tDaggers 🗡 - ${inv[message.author.id].dagger}\n\tSwords ⚔ - ${inv[message.author.id].sword}\n\tMaces ✴ - ${inv[message.author.id].mace}`);
   message.channel.send(botEmbed);
 }
 
