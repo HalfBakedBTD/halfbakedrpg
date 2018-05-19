5abd564c4b2917096439088178bed711b5ac6f2a
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-19 19:52:07 -0400

    Update quest.js

--- commands/quest.js
+++ commands/quest.js
@@ -6,9 +6,24 @@ let monsters = require("../monsters.json");
 module.exports.run = async (bot, message, args) => {
   if(!users[message.author.id]) return message.reply("Please use 'create' to make a charecter before using this command!");
   if (monsters[message.author.id].name !== '') {
+    let die = Math.floor(Math.random() * 5) + 1;
+    let attackEval = users[message.author.id].str + users[message.author.id].con;
+    let strength = attackEval / 5;
+    Math.round(strength);
+    let damage = die + strength;
+    monsters[message.author.id].hp = monsters[message.author.id].hp - damage
+    
+    let dieM = Math.floor(Math.random() * 5) + 1;
+    let attackEvalM = monsters[message.author.id].str + monsters[message.author.id].con;
+    let strengthM = attackEvalM / 5;
+    Math.round(strengthM);
+    let damageM = dieM + strengthM;
+    users[message.author.id].hp = users[message.author.id].hp - damageM
+    
     let botEmbed = new Discord.RichEmbed()
     .setColor('#27ae60')
-    .setDescription(`**Name:** ${message.author.username}\n**Health:** ${users[message.author.id].hp}/${users[message.author.id].maxhp}\n\n**Opponent:** ${monsters[message.author.id].name}\n**Health:** ${monsters[message.author.id].hp}/${monsters[message.author.id].maxhp}`); 
+    .setDescription(`**Name:** ${message.author.username}\n**Health:** ${users[message.author.id].hp}/${users[message.author.id].maxhp}\n+dealt ${die} damage.\n\n**Opponent:** ${monsters[message.author.id].name}\n**Health:** ${monsters[message.author.id].hp}/${monsters[message.author.id].maxhp}\n-took ${dieM} damage.`)
+    .setFooter(`${message.author.username}'s battle against ${monsters[message.author.id].name}.`, message.author.displayAvatarURL);
     message.channel.send(botEmbed);
   }
 }
