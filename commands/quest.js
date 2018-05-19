1dc5a50af6d45d1f050c8824d07b4a8ba376e23f
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-19 19:10:55 -0400

    Update quest.js

--- commands/quest.js
+++ commands/quest.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
   if (monsters[message.author.id].name !== '') {
     let botEmbed = new Discord.RichEmbed()
     .setColor('#27ae60')
-    .setDescription(`**Name:** ${users[message.author.id].name}\n**Health:** ${users[message.author.id].hp}/${users[message.author.id].maxhp}\n\n**Opponent:** ${monsters[message.author.id].name}\n**Health:** ${monsters[message.author.id].hp}/${monsters[message.author.id].maxhp}`); 
+    .setDescription(`**Name:** ${message.author.username}\n**Health:** ${users[message.author.id].hp}/${users[message.author.id].maxhp}\n\n**Opponent:** ${monsters[message.author.id].name}\n**Health:** ${monsters[message.author.id].hp}/${monsters[message.author.id].maxhp}`); 
     message.channel.send(botEmbed);
   }
 }
