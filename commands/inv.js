cd565f3ac66f324e240ce8b26144f914d7a03c7c
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-19 16:00:40 -0400

    Update inv.js

--- commands/inv.js
+++ commands/inv.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if(!inv[message.author.id]) return message.reply("Please use 'create' to make a charecter before using this command!");
   let botEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
-  .setDescription(`${message.author.username}'s inventory:\n\tApples - ${inv[message.author.id].apple}\n\tGrapes - ${inv[message.author.id].grapes}\n\tTomatoes - ${inv[message.author.id].tomato}\n\tCorn - ${inv[message.author.id].corn}\n\tEggs - ${inv[message.author.id].egg}`);
+  .setDescription(`**${message.author.username}'s inventory:**\n\tApples - ${inv[message.author.id].apple}\n\tGrapes - ${inv[message.author.id].grape}\n\tTomatoes - ${inv[message.author.id].tomato}\n\tCorn - ${inv[message.author.id].corn}\n\tEggs - ${inv[message.author.id].egg}`);
   message.channel.send(botEmbed);
 }
 
