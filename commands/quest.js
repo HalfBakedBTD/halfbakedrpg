7a17c84f2418ce535236929586340a175d7446a0
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-19 19:09:02 -0400

    Update quest.js

--- commands/quest.js
+++ commands/quest.js
@@ -1,10 +1,16 @@
 const Discord = require("discord.js");
 let users = require("../users.json");
 let inv = require("../inv.json");
+let monsters = require("../monsters.json");
 
 module.exports.run = async (bot, message, args) => {
   if(!users[message.author.id]) return message.reply("Please use 'create' to make a charecter before using this command!");
-  message.channel.send(`***__Coming Soon__...***`)
+  if (monsters[message.author.id].name !== '') {
+    let botEmbed = new Discord.RichEmbed()
+    .setColor('#27ae60')
+    .setDescription(`**Name:** ${users[message.author.id].name}\n**Health:** ${users[message.author.id].hp}/${users[message.author.id].maxhp}\n\n**Opponent:** ${monsters[message.author.id].name}\n**Health:** ${monsters[message.author.id].hp}/${monsters[message.author.id].maxhp}`); 
+    message.channel.send(botEmbed);
+  }
 }
 
 module.exports.help = {
