84fd370d446efaeea3963cd8b8c5ebda8c33ef1a
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-19 12:47:40 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
   //message.reply("\n💻 My Website: https://jasondw242.wixsite.com/rambunctious\n\n🏝 My server: https://discord.gg/REkAA2X\n\n⚙See my commands list at: https://jasondw242.wixsite.com/rambunctious/commands\n\n`WARNING` due to the sign up feature on the webpage some browsers have interperated it as a webpage to steal personal info. This is just a browser precaution so feel free to ignore it.");
     let botEmbed = new Discord.RichEmbed()
     .setColor('#fff200')
-    .addField("My Commands:", "...");
+    .addDescription("**My Commands:**\n**help** - shows this...");
     message.channel.send(botEmbed);
 }
 
