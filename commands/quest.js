e59e9d7bc53c312a7a60f95d0143dc1fdd883a10
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-19 20:29:01 -0400

    Update quest.js

--- commands/quest.js
+++ commands/quest.js
@@ -28,7 +28,6 @@ module.exports.run = async (bot, message, args) => {
     
     if (monsters[message.author.id].hp < 1) {
       users[message.author.id].quests = users[message.author.id].quests + 1
-      monsters[message.author.id].name = ''
       let dieGold = Math.floor(Math.random() * 5) + 1;
       dieGold = dieGold * users[message.author.id].lvl
       users[message.author.id].gold = users[message.author.id].gold + dieGold
@@ -36,9 +35,11 @@ module.exports.run = async (bot, message, args) => {
       users[message.author.id].xp = users[message.author.id].xp + dieXP
       let winEmbed = new Discord.RichEmbed()
       .setColor('#2ecc71')
-      .setDescription(`**Quest Complete!**\n+{dieGold} gold.\n+${dieXP} expirience.\n+1 quests complete.`)
+      .setDescription(`**Quest Complete!**\n+${dieGold} gold.\n+${dieXP} expirience.\n+1 quests complete.`)
       .setFooter(`${message.author.username} has won the battle against ${monsters[message.author.id].name}.`, message.author.displayAvatarURL);
       message.channel.send(winEmbed);
+      monsters[message.author.id].name = ''
+      return;
     }
     if (users[message.author.id].hp < 1) {
       users[message.author.id].deaths = users[message.author.id].deaths + 1
@@ -50,6 +51,7 @@ module.exports.run = async (bot, message, args) => {
       .setDescription(`**Quest Failed!**\n+1 deaths.\n-${goldLoss} gold.`)
       .setFooter(`${message.author.username} has lost the battle against ${monsters[message.author.id].name}.`, message.author.displayAvatarURL);
       message.channel.send(winEmbed);
+      return;
     }
   }
 }
