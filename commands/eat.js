c4f6c3e34bebe8ca45b22bbe4c2361fdaaf06481
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-19 22:22:55 -0400

    Update eat.js

--- commands/eat.js
+++ commands/eat.js
@@ -7,7 +7,12 @@ module.exports.run = async (bot, message, args) => {
   if(!users[message.author.id]) return message.reply("Please use 'create' to make a charecter before using this command!");
   let item = args[0];
   if(!item) return message.channel.send("What do you want to eat? Let me know in the command! (Example: `eat egg`)");
-  message.channel.send("You ate something...")
+  if (item === '') {
+     (inv[message.author.id]. < 1)
+      message.channel.send(`You do not own a ${item} which you can eat.`);
+      return;
+  }  
+  }
 }
 
 module.exports.help = {
