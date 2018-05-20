f3049b1ae91b71e0cddceb74a7eac015b4612b1a
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-20 00:20:15 -0400

    Create assign.js

---
+++ commands/assign.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+let users = require("../users.json");
+let inv = require("../inv.json");
+let monsters = require("../monsters.json");
+
+module.exports.run = async (bot, message, args) => {
+  if(!users[message.author.id]) return message.reply("Please use 'create' to make a charecter before using this command!");
+  message.channel.send("Coming soon...")
+}
+
+module.exports.help = {
+  name: "assign"
+}
