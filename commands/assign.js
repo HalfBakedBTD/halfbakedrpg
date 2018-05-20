05c7c99d0b9f6d14ca3e7831290e77f8a8478a0b
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-20 15:16:04 -0400

    Update assign.js

--- commands/assign.js
+++ commands/assign.js
@@ -5,7 +5,14 @@ let monsters = require("../monsters.json");
 
 module.exports.run = async (bot, message, args) => {
   if(!users[message.author.id]) return message.reply("Please use 'create' to make a charecter before using this command!");
-  message.channel.send("Coming soon...")
+  let amount = args[0];
+  let type = args[1];
+  if(!amount) return message.channel.send("You have provided invalid arguments! Use the command by providing an amount of points to assign then provide what to assign it to. (Example: `assign 1 strength`)");
+  if(!type) return message.channel.send("You have provided invalid arguments! Use the command by providing an amount of points to assign then provide what to assign it to. (Example: `assign 1 strength`)");
+  if(!amount) return message.channel.send("You have provided invalid arguments! Use the command by providing an amount of points to assign then provide what to assign it to. (Example: `assign 1 strength`)");
+  if(isNaN(args[0])) return message.channel.send("You have provided invalid arguments! Use the command by providing an amount of points to assign then provide what to assign it to. (Example: `assign 1 strength`)");
+  if(parseInt(args[0]) < 1) return message.reply("you can't assign less than 1 point at  time!");
+  if(parseInt(args[0]) > 10) return message.reply("you can't assign more than 10 points at  time!");
 }
 
 module.exports.help = {
