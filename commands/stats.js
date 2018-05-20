eaf1357a108b1e7d01a3d4d0f50315d946eb7f1f
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-19 22:45:07 -0400

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   let userEmbed = new Discord.RichEmbed()
   .setThumbnail((message.author.displayAvatarURL))
   .setColor('#27ae60')
-  .setDescription(`**Name:** ${message.author.username}\n**Race:** ${users[message.author.id].race}\n\n**Health:** ${users[message.author.id].hp}\n**Gold:** ${users[message.author.id].gold}\n**Level:** ${users[message.author.id].lvl}\n**Experience:** ${users[message.author.id].xp}\n\n**Strength:** ${users[message.author.id].str}\n**Dexterity:** ${users[message.author.id].dex}\n**Concentration:** ${users[message.author.id].con}\n**Inteligence:** ${users[message.author.id].int}\n**Wisdom:** ${users[message.author.id].wis}\n**Charm:** ${users[message.author.id].cha}\n\n**Quests:** ${users[message.author.id].quests}\n**Deaths:** ${users[message.author.id].deaths}`)
+  .setDescription(`**Name:** ${message.author.username}\n**Race:** ${users[message.author.id].race}\n\n**Health:** ${users[message.author.id].hp}/${user[message.author.id].maxhp}\n**Gold:** ${users[message.author.id].gold}\n**Level:** ${users[message.author.id].lvl}\n**Experience:** ${users[message.author.id].xp}\n\n**Strength:** ${users[message.author.id].str}\n**Dexterity:** ${users[message.author.id].dex}\n**Concentration:** ${users[message.author.id].con}\n**Inteligence:** ${users[message.author.id].int}\n**Wisdom:** ${users[message.author.id].wis}\n**Charm:** ${users[message.author.id].cha}\n\n**Quests:** ${users[message.author.id].quests}\n**Deaths:** ${users[message.author.id].deaths}`)
   
   message.channel.send(userEmbed)
 }
