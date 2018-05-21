fa2cb27e3d983931b41af982fd934fba78f5593b
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-20 22:22:25 -0400

    Update weapon.js

--- commands/weapon.js
+++ commands/weapon.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   if(!users[message.author.id]) return message.channel.send("Use 'create' to create a charecter before using `weapon` command!");
   let botEmbed = new Discord.RichEmbed()
   .setColor('#27ae60')
-  .setDescription(`You are currently using ${users[message.author.id].weapon} in battles. You can obtain new weapons by killing the bosses.`);
+  .setDescription(`You are currently using ${users[message.author.id].weapon} in battles.`);
   message.channel.send(botEmbed);
 }
 
