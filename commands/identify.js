e9cb8559000934e8fee60eed6aaf7c57f4cdbf05
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-24 22:29:33 -0400

    Update identify.js

--- commands/identify.js
+++ commands/identify.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   if (message.author.id === '284137818895417344') {
-    message.channel.send(`${message.author.username}, you are an admin.`)
+    message.channel.send(`[${message.author.username} - ADMIN]`)
   } else if (message.author.id === '275831434772742144') {
-    message.channel.send(`${message.author.username}, you are an admin.`)
+    message.channel.send(`[${message.author.username} - ADMIN]`)
   } else {
-    message.channel.send(`${message.author.username}, you are irrelevant.`)
+    message.channel.send(`[${message.author.username} - IRRELEVANT]`)
   }
 }
 
