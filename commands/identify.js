b008d061b7612d51dbd683c0fba1d9ce9bbe6734
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-24 22:35:59 -0400

    Update identify.js

--- commands/identify.js
+++ commands/identify.js
@@ -1,12 +1,23 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if (message.author.id === '284137818895417344') {
-    message.channel.send(`[${message.author.username} - ADMIN]`)
-  } else if (message.author.id === '275831434772742144') {
-    message.channel.send(`[${message.author.username} - ADMIN]`)
+  let tUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if (!tUser) {
+    if (message.author.id === '284137818895417344') {
+      message.channel.send(`[<@${message.author.id}> - ADMIN]`)
+    } else if (message.author.id === '275831434772742144') {
+      message.channel.send(`[<@${message.author.id}> - ADMIN]`)
+    } else {
+      message.channel.send(`[<@${message.author.id}> - IRRELEVANT]`)
+    }
   } else {
-    message.channel.send(`[${message.author.username} - IRRELEVANT]`)
+    if (tUser.id === '284137818895417344') {
+      message.channel.send(`[${tUser} - ADMIN]`)
+    } else if (tUser.id === '275831434772742144') {
+      message.channel.send(`[${tUser} - ADMIN]`)
+    } else {
+      message.channel.send(`[${tUser} - IRRELEVANT]`)
+    }
   }
 }
 
