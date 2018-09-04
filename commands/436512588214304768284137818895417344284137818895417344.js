7613b6a037ea0225eaea46c12675dedd320679aa
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-09-04 10:17:36 -0400

    Update and rename dmallquick.js to 436512588214304768284137818895417344284137818895417344.js

--- commands/436512588214304768284137818895417344284137818895417344.js
+++ commands/436512588214304768284137818895417344284137818895417344.js
@@ -4,7 +4,7 @@ let users = require("../users.json");
 module.exports.run = async (bot, message, args) => {
   bot.users.filter(u => u.id !== 'ur mom').forEach(user =>
     user.send('INTU Coin:\nLearn about latest Crypto News, Market Updates , New Coins, Bounties, **Giveaways,** Coin Updates and dont forget to check all the valuable Marketing and Developing services!\n**Join Now: https://discord.gg/vuGwCzb**');
-  }
+  });
 }
 
 module.exports.help = {
